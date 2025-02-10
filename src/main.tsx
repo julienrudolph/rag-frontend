2c0385a23257be22677bcdda5923cc063d90f88c
Julien <julien.rudolph@outlook.com>
2025-02-10 13:28:38 +0100

    reaact-oidc

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode, useEffect, useState } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { UserManager, WebStorageStateStore } from "oidc-client-ts";
+import { AuthProvider } from 'react-oidc-context'
 
 const config = {
   authority: "https://sts.cducsu.de/adfs", // Ersetze durch deine ADFS-URL
@@ -11,38 +11,15 @@ const config = {
   response_type: "code", // Code-Flow für sichere Authentifizierung
   scope: "openid profile email", // Gewünschte Scopes
   post_logout_redirect_uri: "http://localhost:3000/",
-  userStore: new WebStorageStateStore({ store: window.localStorage }),
 };
 
-const userManager = new UserManager(config);
-
-function Login() {
-  const [user, setState] = useState(null);
-
-  useEffect(() => {
-    userManager.getUser().then((loadedUser) => {
-      if (loadedUser) {
-        setState(loadedUser as any);
-      }
-    });
-  }, []);
-
-  const login = () => {
-    userManager.signinRedirect();
-  };
-
+export default function Login() {
   return (
-    <div>
-      <h1>React ADFS OIDC Authentication</h1>
-      {user ? (
-        <div>
-          <p>Welcome, {user}</p>
-          <App/>
-        </div>
-      ) : (
-        <button onClick={login}>Login</button>
-      )}
-    </div>
+    <AuthProvider {...config}>
+      <StrictMode>
+        <App/>
+      </StrictMode>
+    </AuthProvider>
   );
 }
 
